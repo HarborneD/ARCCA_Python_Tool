bbc74a2e41527a34458d4e1b49afc165e0f205a4
harborned <d.harborne@outlook.com>
2019-02-09 18:15:04 +0000

    gitignore credentials

--- ARCCAPythonTool.py
+++ ARCCAPythonTool.py
@@ -1,3 +1,7 @@
+import base64
+import paramiko
+
+import json
 #fyp_scw1427
 #dais_scw1077
 
@@ -26,3 +30,21 @@
 #directory of remote location
 
 #fetch files from remote location
+
+host = "hawklogin.cf.ac.uk"
+
+credentials_json = None
+with open("credentials.json","r") as f:
+    credentials_json = json.load(f)
+
+print(credentials_json["username"])
+
+
+# key = paramiko.RSAKey(data=base64.b64decode(b'AAA...'))
+# client = paramiko.SSHClient()
+# client.get_host_keys().add('ssh.example.com', 'ssh-rsa', key)
+# client.connect('ssh.example.com', username='strongbad', password='thecheat')
+# stdin, stdout, stderr = client.exec_command('ls')
+# for line in stdout:
+#     print('... ' + line.strip('\n'))
+# client.close()
