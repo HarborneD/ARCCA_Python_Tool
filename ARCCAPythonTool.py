1eb0b8ff5ceb65ab0361cbbe8366c6a37d09e054
harborned <d.harborne@outlook.com>
2019-02-09 17:34:16 +0000

    inital commit

---
+++ ARCCAPythonTool.py
@@ -0,0 +1,28 @@
+#fyp_scw1427
+#dais_scw1077
+
+#login
+
+
+###jobs
+#submit single job
+
+#submit batch of jobs
+
+#check job status
+
+#restart failed jobs
+
+#check expected start time for jobs
+
+
+
+##remote storage (scratch/home)
+#get quota of home
+    #myquota
+
+#send files to remote location
+
+#directory of remote location
+
+#fetch files from remote location
